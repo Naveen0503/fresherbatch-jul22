18b44f5cca2b6be484ce51371a7fc52a4c2fb84b
Naveen0503 <109223016+Naveen0503@users.noreply.github.com>
2022-08-22 16:22:52 +0530

    Uploading assignment3

---
+++ ASP.NET MVC Assignments/Assignment_3(EF)/Assignment_3(EF)/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using DataAccessLayer;
+using BusinessLayer;
+
+
+namespace Assignment_3_EF_
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer<DataAccessLayer.CustomerDBContext>(null);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
